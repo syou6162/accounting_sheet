b91bdce7387089fdda9aabeeba1c3b3c11898833
Yasuhisa Yoshida <syou6162@gmail.com>
2017-06-26 21:39:49 +0900

    誰の投稿かも記録するように修正

--- dev/main.ts
+++ dev/main.ts
@@ -2,10 +2,12 @@ import Spreadsheet = GoogleAppsScript.Spreadsheet.Spreadsheet;
 declare var SlackApp: any;
 
 class Purchase {
+  user_name: string;
   yen: number;
   content: string;
   date: Date;
-  constructor(yen: number, content: string) {
+  constructor(user_name: string, yen: number, content: string) {
+    this.user_name = user_name;
     this.yen = yen;
     this.content = content;
     this.date = new Date();
@@ -13,20 +15,22 @@ class Purchase {
 }
 
 class IncommingParameter {
+  user_name: string;
   text: string;
   token: string;
-  constructor(text: string, token: string) {
+  constructor(user_name: string, text: string, token: string) {
+    this.user_name = user_name;
     this.text = text;
     this.token = token;
   }
 }
 
-function parseLine(line: string) {
+function parseLine(line: string): [number, string] {
   let lineRegex = /^(\d+):\s?(.*?)$/mg;
   let match = lineRegex.exec(line);
   let yen = parseInt(match![1]);
   let content = match![2];
-  return new Purchase(yen, content);
+  return [yen, content];
 }
 
 class AccountingSheet {
@@ -45,7 +49,7 @@ class AccountingSheet {
   }
 
   slackParam2IncommingParameter(slackParam: any): IncommingParameter {
-    return new IncommingParameter(slackParam.parameter.text, slackParam.parameter.token);
+    return new IncommingParameter(slackParam.parameter.user_name, slackParam.parameter.text, slackParam.parameter.token);
   }
 
   doPost(slackParam: any) {
@@ -55,7 +59,8 @@ class AccountingSheet {
       throw new Error("invalid token.");
     }
     let message: string = p.text;
-    let purchase = parseLine(message);
+    let [yen, content] = parseLine(message);
+    let purchase = new Purchase(p.user_name, yen, content);
     this.writeCell(purchase);
     let app = SlackApp.create(this.slackToken);
     let sendMessage: string = purchase.content + "(" + purchase.yen + "円)を受け付けました。修正するには " + this.spreadsheetUrl + " を開いてください";
@@ -67,9 +72,10 @@ class AccountingSheet {
     let lastRow = sheet.getLastRow();
     for (let i = lastRow; i >= 1; i--) {
       if (sheet.getRange(i, 1).getValue() != '') {
-        sheet.getRange(i + 1, 1).setValue(purchase.date);
-        sheet.getRange(i + 1, 2).setValue(purchase.content);
-        sheet.getRange(i + 1, 3).setValue(purchase.yen);
+        sheet.getRange(i + 1, 1).setValue(purchase.user_name);
+        sheet.getRange(i + 1, 2).setValue(purchase.date);
+        sheet.getRange(i + 1, 3).setValue(purchase.content);
+        sheet.getRange(i + 1, 4).setValue(purchase.yen);
         break;
       }
     }
@@ -88,6 +94,7 @@ function init() {
   let token = PropertiesService.getScriptProperties().getProperty("SLACK_VERIFY_TOKEN")
   let slackParam = {
     parameter: {
+      user_name: "test_user",
       text: "111: test",
       token: token
     }
